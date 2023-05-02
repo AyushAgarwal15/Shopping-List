705f43e77453602fa7384e109581a05a00c3f799
Ayush Agarwal <aa2503452@gmail.com>
2023-05-03 03:25:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ onValue(shoppingListInDB, function (snapshot) {
     }
   } else {
     shoppingListEl.innerHTML = `<div class="sad-cat-container">
-                                      <img src="./assets/cat2.avif">
+                                      <img src="./assets/sad-cat.avif">
                                       <h3>No item here ...</h3>
                                 </div>`;
   }
